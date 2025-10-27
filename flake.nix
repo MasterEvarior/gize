c5711f3b958c2f9bbc8ab00e8a8588029bb403ac
Giannin <contact@giannin.ch>
2025-10-27 19:33:32 +0100

    Add flake checks

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     let
       x86 = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${x86}";
+      lib = pkgs.lib;
     in
     {
       devShells."${x86}".default = pkgs.mkShellNoCC {
@@ -47,6 +48,63 @@
         version = "v1.1.0";
         src = ./.;
         vendorHash = "sha256-/OzNsgU3VNnkL9sXDoZahJ7fMqoYCEmstnNnGvmF03A=";
+      };
+
+      checks."${x86}" = {
+        tests = pkgs.buildGoModule {
+          name = "tests";
+          src = ./.;
+          doCheck = true;
+
+          nativeBuildInputs = [
+            pkgs.go
+          ];
+
+          checkPhase = ''
+            ${lib.getExe pkgs.go} test ./...
+          '';
+
+          installPhase = ''
+            mkdir "$out"
+          '';
+
+          vendorHash = "sha256-/OzNsgU3VNnkL9sXDoZahJ7fMqoYCEmstnNnGvmF03A=";
+        };
+
+        formatting = pkgs.buildGoModule {
+          name = "formatting";
+          src = ./.;
+          doCheck = true;
+
+          nativeBuildInputs = with pkgs; [
+            go
+            golangci-lint
+
+            treefmt
+            beautysh
+            mdformat
+            yamlfmt
+            jsonfmt
+            deadnix
+            nodePackages.prettier
+            nixfmt-rfc-style
+          ];
+
+          checkPhase = ''
+            export HOME=$PWD
+            export GOCACHE=$PWD/.go-cache
+            mkdir -p $GOCACHE
+
+            ${lib.getExe pkgs.golangci-lint} run
+            ${lib.getExe pkgs.treefmt} --ci
+          '';
+
+          installPhase = ''
+            mkdir "$out"
+          '';
+
+          vendorHash = "sha256-/OzNsgU3VNnkL9sXDoZahJ7fMqoYCEmstnNnGvmF03A=";
+        };
       };
     };
 }
